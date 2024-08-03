bb334d8beb7436cb843d69e0b73b2f6946d6b289
KanonKC <kanonkce@gmail.com>
2024-08-03 21:16:45 +0700

    Respect ratio and error handling

--- src/commands/twitch-emote.ts
+++ src/commands/twitch-emote.ts
@@ -7,22 +7,45 @@ import sharp from "sharp";
 
 export const TwitchEmote: SlashCommand = {
 	name: "twitch-emote",
-	description: "Replies with pong!!!",
+	description: "Transform a Discord emote into a 112x112 PNG image",
 	options: [
 		{
 			name: "emote",
-			description: "Put your discord emote here",
+			description: "Put your Discord emote here",
 			type: ApplicationCommandOptionType.String,
 			required: true,
+		},
+		{
+			name: "respect-ratio",
+			description: "Select this to True if you want to keep the aspect ratio of the image",
+			type: ApplicationCommandOptionType.Boolean,
+			required: false,
 		},
 	],
 
 	async onCommandExecuted(interaction) {
 		const emoteOption = interaction.options.get("emote");
+		const respectRatioOption = interaction.options.get("respect-ratio");
 
         if (!emoteOption) return;
 
         const emote = emoteOption.value as string;
+        const respectRatio = respectRatioOption?.value as boolean;
+
+        if (emote.split(":").length < 3) {
+            await interaction.reply({
+                content: "Invalid emote format. Please type only an emote.",
+                files: [
+                    {
+                        attachment: 'src/assets/how-to-use.png',
+                        name: `how-to-use.png`
+                    }
+                ],
+                ephemeral: true,
+            });
+            return;
+        }
+
         const emoteName = emote.split(":")[1];
         const emoteId = emote.split(":")[2].split(">")[0];
 
@@ -32,11 +55,28 @@ export const TwitchEmote: SlashCommand = {
             responseType: 'arraybuffer',
           });
 
-        const img = await sharp(imageResponse.data).toFormat('png').resize(112, 112).toBuffer();
-        await sharp(img).toFile('src/resources/test.png');
+        const image = sharp(imageResponse.data).toFormat('png') //.resize(112, 112).toBuffer();
+
+        let imageBuffer = null
+
+        if (respectRatio) {
+
+            const { width, height } = await image.metadata();
+            if (!width || !height) return;
+            else if (width > height) {
+                imageBuffer = await image.resize({ width: 112 }).toBuffer();
+            }
+            else {
+                imageBuffer = await image.resize({ height: 112 }).toBuffer();
+            }
+        }
+        else {
+            imageBuffer = await image.resize({ width: 112, height: 112 }).toBuffer();
+        }
+            
+        await sharp(imageBuffer).toFile('src/resources/test.png');
 
         await interaction.reply({
-            content: "Here is your emote!\n(PNG File with 112x112 Pixels Size)",
             files: [
                 {
                     attachment: 'src/resources/test.png',
